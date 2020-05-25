da084e1f3399cc90187588805c98d8f198200c06
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-25 13:18:36 +0900

    스플래쉬 화면 추가 + 이 화면을 시작점으로 변경

---
+++ app/src/main/java/kr/co/tjoeun/baseballgame_20200525/SplashActivity.java
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.baseballgame_20200525;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
